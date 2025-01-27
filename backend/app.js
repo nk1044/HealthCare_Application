c6b638f4f1a65719efbbdb377db90442171bea32
abhy <b23cs1085@iitj.ac.in>
2025-01-27 19:30:10 +0530

    added admin pannel

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,33 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import 'dotenv/config';
 import authRouter from './src/routes/auth.route.js';
+import AdminJS from 'adminjs'
+import AdminJSExpress from '@adminjs/express'
+import * as AdminJSMongoose from '@adminjs/mongoose'
+import { User } from './src/models/user.model.js';
+
+
+AdminJS.registerAdapter({
+    Resource: AdminJSMongoose.Resource,
+    Database: AdminJSMongoose.Database,
+})
+
+const adminOptions = {
+    resources: [User],
+}
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const admin = new AdminJS(adminOptions)
+
+const adminRouter = AdminJSExpress.buildRouter(admin)
+app.use(admin.options.rootPath, adminRouter)
+
+
+
 
 app.use(cookieParser());
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
     origin: 'https://remotecare.netlify.app',
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 import app from './app.js';
 import {connectDB} from './src/config/db.js';
 import 'dotenv/config';
+import AdminJS from 'adminjs'
+import AdminJSExpress from '@adminjs/express'
+
 
 
 const port = process.env.PORT || 3000;
 
 connectDB().then(()=>{
+    
     app.listen(port, () => {
         console.log(`Server is listening on port ${port}`);
     });
