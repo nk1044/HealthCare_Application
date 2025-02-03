e16f009ff9e839b3198fd3e3e6a855075739b00c
NeerajKumar-1044 <neerajkumar.cs1044@gmail.com>
2025-02-03 20:22:46 +0530

    fix(ThirdCard): adjust image styling and add overflow handling for better layout

--- HealthCare_App/src/Components/ThirdCard.jsx
+++ HealthCare_App/src/Components/ThirdCard.jsx
@@ -7,8 +7,9 @@ function ThirdCard({
 }) {
     return (
 
-        <div className=" bg-white border w-[150px] h-[200px] border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 p-2 transition-transform transform hover:scale-105">
-            <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={image_url} alt="IMG" />
+        <div className=" bg-white border w-[150px] h-[200px] border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 p-2 
+        transition-transform transform hover:scale-105 overflow-hidden">
+            <img className="w-full h-[80%] rounded-t-lg md:h-[80%] md:w-full md:rounded-none md:rounded-s-lg" src={image_url} alt="IMG" />
             <div className="flex flex-col justify-between text-center ">
                 <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900">{name}</h5>
             </div>
