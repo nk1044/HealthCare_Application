d7fc42d18834d02b0d7a76aaaf0a52f7022ac773
NeerajKumar-1044 <neerajkumar.cs1044@gmail.com>
2025-03-12 00:04:16 +0530

    feat(backend): add Prometheus metrics endpoint and update Dockerfile for improved setup

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,33 @@
 import app, { server } from './app.js';
 import { connectDB } from './src/config/db.js';
 import 'dotenv/config';
+import client from 'prom-client';
 
-const port = process.env.PORT || 3000;
-
+const port = process.env.PORT || 9000;
+const register = new client.Registry();
 
 app.get('/health-check', (req, res) => {
     res.send('Server is running healthy 👍');
 });
+
+
+client.collectDefaultMetrics({ register });
+app.get('/metrics', async (req, res) => {
+    // Set the correct content type header
+    res.setHeader('Content-Type', register.contentType);
+    
+    // Get all metrics from the registry
+    const metrics = await register.metrics();
+    
+    // Send metrics as response
+    res.send(metrics);
+  });
+
+
 connectDB()
     .then(async () => {
 
-        server.listen(port, () => {
+        server.listen(port,'0.0.0.0', () => {
             console.log(`Server is listening on port ${port}`);
         });
     })
