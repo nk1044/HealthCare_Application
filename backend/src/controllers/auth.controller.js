29798e59a8aca68df63492eac94cd48bae72fe8e
NeerajKumar-1044 <neerajkumar.cs1044@gmail.com>
2025-03-28 16:31:32 +0530

    fix(auth): update cookie options for production environment

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,15 +2,19 @@ import { User } from '../models/user.model.js';
 import { verifyGoogleToken } from '../config/google.js';
 // import {redis} from '../config/redis.js'
 
+// const options = {
+//   httpOnly: true,
+//   secure: process.env.NODE_ENV === "production",
+//   sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
+//   maxAge: 24 * 60 * 60 * 1000,
+// };
 const options = {
-  httpOnly: true,
   secure: process.env.NODE_ENV === "production",
   sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
-  maxAge: 24 * 60 * 60 * 1000,
 };
 
 
-
+ 
 const GenerateToken = async (userId) => {
   const user = await User.findById(userId);
   const AccessToken = await user.GenerateAccessToken();
