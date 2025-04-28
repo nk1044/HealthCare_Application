234db67eeaaa25166d6d3fe0f60921ced241edb1
NeerajKumar-1044 <neerajkumar.cs1044@gmail.com>
2025-04-28 21:42:23 +0530

    feat: add Docker configuration files for backend, frontend, and database services

--- HealthCare_App/vite.config.js
+++ HealthCare_App/vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    port: 3000,
+    strictPort: true,
+   },
+   server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true,
+    strictPort: true,
+    port: 5173,
+  }
 })
